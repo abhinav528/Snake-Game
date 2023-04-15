74e82fa6499325fa891cee2553785251f11505a4
abhinav528 <96528243+abhinav528@users.noreply.github.com>
2023-04-15 16:42:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,153 @@
+let inputDir = { x: 0, y: 0 };
+const foodSound = new Audio('./SoundsUsed/food.mp3');
+const gameOverSound = new Audio('./SoundsUsed/gameover.mp3');
+const moveSound = new Audio('./SoundsUsed/move.mp3');
+const musicSound = new Audio('./SoundsUsed/music.mp3');
+let speed = 10;
+let score = 0;
+let lastPaintTime = 0;
+let snakeArr = [
+    { x: 13, y: 15 }
+];
+
+food = { x: 6, y: 7 };
+
+// Game Functions
+function main(ctime) {
+    window.requestAnimationFrame(main);
+    // console.log(ctime)
+    if ((ctime - lastPaintTime) / 1000 < 1 / speed) {
+        return;
+    }
+    lastPaintTime = ctime;
+    gameEngine();
+};
+
+
+function isCollide() {
+    // if ypu bump into yourself
+    for (let i = 1; i < snakeArr.length; i++) {
+        if (snakeArr[i].x === snakeArr[0].x && snakeArr[i].y === snakeArr[0].y) {
+            return true;
+        }
+    }
+
+    // if you bump into the wall
+    if (snakeArr[0].x >= 18 || snakeArr[0].x <= 0 || snakeArr[0].y >= 18 || snakeArr[0].y <= 0) {
+        return true;
+    }
+
+
+
+}
+
+
+
+
+function gameEngine() {
+    // Part 1: Updating the snake array & Food
+    if (isCollide(snakeArr)) {
+        gameOverSound.play();
+        musicSound.pause();
+        inputDir = { x: 0, y: 0 };
+        alert("Game Over. Press any key to play again!");
+        snakeArr = [{ x: 13, y: 15 }];
+        musicSound.play();
+        score = 0;
+    }
+
+
+    // If you have eaten the food, increment the score and regenerate the food
+    if (snakeArr[0].y === food.y && snakeArr[0].x === food.x) {
+        foodSound.play();
+        score += 1;
+        scoreBox.innerHTML = "Score: " + score;
+        snakeArr.unshift({ x: snakeArr[0].x + inputDir.x, y: snakeArr[0].y + inputDir.y });        // here we are shifting the head oof snake in the direction   of its movement, basically increasing its size
+
+        let a = 2; // a and b are variables between which we want our random number , i.e the coordinates of food
+        let b = 16;
+        food = { x: Math.round(a + (b - a) * Math.random()), y: Math.round(a + (b - a) * Math.random()) }
+    }
+
+
+
+    // Moving the snake
+
+    for (let i = snakeArr.length - 2; i >= 0; i--) {    // we are taking its length from its tail , i.e reverse
+
+        snakeArr[i + 1] = { ...snakeArr[i] };    // we have to make a new object here
+
+    }
+
+    snakeArr[0].x += inputDir.x    // snake's head
+    snakeArr[0].y += inputDir.y
+
+
+    // Part 2: Display the snake and Food
+    // Display the snake
+    board.innerHTML = "";
+    snakeArr.forEach((e, index) => {
+        snakeElement = document.createElement('div');
+        snakeElement.style.gridRowStart = e.y;
+        snakeElement.style.gridColumnStart = e.x;
+
+        if (index === 0) {
+            snakeElement.classList.add('head');
+        }
+        else {
+            snakeElement.classList.add('snake');
+        }
+        board.appendChild(snakeElement);
+    });
+
+
+    // Display the food
+    foodElement = document.createElement('div');
+    foodElement.style.gridRowStart = food.y;
+    foodElement.style.gridColumnStart = food.x;
+    foodElement.classList.add('food')
+    board.appendChild(foodElement);
+
+
+}
+
+
+
+
+
+// Main logic starts here
+
+window.requestAnimationFrame(main);
+window.addEventListener("keydown", e => {
+    inputDir = { x: 0, y: 1 }
+    musicSound.play();
+    moveSound.play();
+    switch (e.key) {
+        case "ArrowUp":
+            console.log("ArrowUp");
+            inputDir.x = 0;
+            inputDir.y = -1;
+            break;
+
+        case "ArrowDown":
+            console.log("ArrowDown");
+            inputDir.x = 0;
+            inputDir.y = 1;
+            break;
+
+        case "ArrowLeft":
+            console.log("ArrowLeft");
+            inputDir.x = -1;
+            inputDir.y = 0;
+            break;
+
+        case "ArrowRight":
+            console.log("ArrowRight");
+            inputDir.x = 1;
+            inputDir.y = 0;
+            break;
+        default:
+            break;
+    }
+
+})
